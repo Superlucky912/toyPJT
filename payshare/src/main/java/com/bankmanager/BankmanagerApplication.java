a91249e7dd0049b5784cf00fcc633aa1f225b677
Superlucky912 <jong3696@gmail.com>
2024-09-04 10:32:34 +0900

    프로젝트 구조 변경
    1. spring-boot 기반 vue 통합 프로젝트 구성
    2. 불필요한 파일 삭제 (indexController.java)
    3. DemoApplication > BankmanagerApplication 으로 이름 수정 및 루트 폴더로 이동
    4. frontend 현행화 > vue build
    
    - 향후 작업
    1. Bankmanager > PayShare 로 프로젝트명 변경 필요

--- payshare/src/main/java/com/bankmanager/BankmanagerApplication.java
+++ payshare/src/main/java/com/bankmanager/BankmanagerApplication.java
@@ -1,15 +1,15 @@
-package com.bankmanager.demo;
+package com.bankmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication(scanBasePackages = "com.bankmanager")
-public class DemoApplication {
+public class BankmanagerApplication {
 
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(BankmanagerApplication.class, args);
 		
 	}
 
--- payshare/vue/webpack/vite.config.js
+++ payshare/vue/webpack/vite.config.js
@@ -5,6 +5,10 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: path.resolve(__dirname, "../../src/main/resources/static"),
+    emptyOutDir: true, // 기존 파일 삭제
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
